55358946b038ff689ee7a31d76ff01bdad526d67
Rhys <rjones.zx@gmail.com>
2018-12-06 20:44:46 +0000

    Fixt

--- src/main/scala/codeforces/CF_525_2_B.scala
+++ src/main/scala/codeforces/CF_525_2_B.scala
@@ -1,34 +1,37 @@
 // date: 04/12/2018
-// tested working online: YES / NO
-
+// tested working online: YES
 package codeforces
 
+import java.io.PrintWriter
 import scala.collection.SortedSet
 
 object CF_525_2_B {
-  type VI = Vector[Int]
-  // try sorting first then keeping tally of amount substracted
 
-
-  def solve(n: Int, k: Int, as: VI): VI = {
-    def loop(pass: Int, xs: Seq[Int], totalSubtracted: Long, out: VI): VI = pass match {
-      case 0 => out
-      case _ => xs match {
-        case Vector() => out ++ Vector.fill(pass)(0)
-        case Vector(a, rest@_*) =>
-          val a1 = (a.toLong - totalSubtracted) max 0
-          loop(pass - 1, rest.dropWhile(_ <= a), totalSubtracted + a1, out :+ a1.toInt)
-      }
-    }
-
-//    val ss = SortedSet(as)
-    loop(k, as.sorted.dropWhile(_==0), 0, Vector.empty)
+/*This problem took ages because it kept exceeding the time limit
+* Hence tried various mutable / imperative solutions - didn't help
+* Needed to use a PrintWriter instead of writing directly to System.out,
+* which approximately halved the runtime*/
+  def solve(n: Int, k: Int, as: Vector[Int]): Seq[Int] = {
+    val xs = SortedSet(as: _*).toStream
+    val xs0 = 0 #:: xs
+    val result: Seq[Int] = (xs0, xs).zipped.map((a, b) => b-a).take(k).padTo(k, 0)
+    result
   }
-  
+
   def main(args: Array[String]) = {
     val io = new IO(System.in); import io._
+//    val n = 100000 // 2060 ms using normal printer; 1007 ms using PrintWriter
+//    val bigTest = for(i <- 1 to n) yield util.Random.nextInt(n) + 1
+//    println(bigTest)
+//    val io = new IO(s"$n $n\n" + bigTest.mkString(" ")); import io._
+//    val t0 = System.currentTimeMillis()
     val solution = solve(int, int, {nextLine; intSeq()})
-    solution foreach println
+    val out = new PrintWriter(System.out)
+    solution foreach out.println
+    out.close()
+//    val t1 = System.currentTimeMillis()
+//    println(s"Time in ms: ${t1-t0}")
+
   }
 
 
