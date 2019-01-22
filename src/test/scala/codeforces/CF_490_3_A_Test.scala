82492bc74ba64bff32ec0739a336fcd0fb04e8d6
Rhys <rjones.zx@gmail.com>
2019-01-22 22:02:12 +0000

    Match 490_3

---
+++ src/test/scala/codeforces/CF_490_3_A_Test.scala
@@ -0,0 +1,26 @@
+package codeforces
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers
+import codeforces.CF_490_3_A._
+
+class CF_490_3_A_Test extends FunSuite with Matchers {
+
+  test("1") {
+    """8 4
+      |4 2 3 1 5 1 6 4
+      |""".solveVal shouldEqual 5
+  }
+
+  test("2") {
+    """5 2
+      |3 1 2 1 3
+      |""".solveVal shouldEqual 0
+  }
+
+  test("3") {
+    """5 100
+      |12 34 55 43 21
+      |""".solveVal shouldEqual 5
+  }
+}
